01b6a06e5327a997023a338085c04aaa24ef65cd
Kwarthys <Kwarthys@gmail.com>
2020-10-02 15:32:10 +0200

    Dash and damages !

---
+++ SlasherMan/Assets/DashAttack.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashAttack : MonoBehaviour
+{
+    public Camera cam;
+
+    public bool isOkay = true;
+
+    public float internalCD = 0.3f;
+    private float lastCast = -1;
+
+    public float speed = 20;
+    public float dashDistance = 7;
+
+    public LayerMask floorLayer;
+    public LayerMask enemiesLayer;
+
+    public int damage = 25;
+
+    private bool dashing = false;
+    private Vector3 dashTarget;
+
+    private Rigidbody rbody;
+
+    private Transform parent;
+
+    private void Start()
+    {
+        rbody = GetComponentInParent<Rigidbody>();
+        parent = transform.parent;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(dashing)
+        {
+            LivingThing l = col.gameObject.GetComponentInParent<LivingThing>();
+            if(l!=null)
+            {
+                l.takeDamage(damage);
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        if(dashing)
+        {
+            parent.position += (dashTarget - parent.position) * speed * Time.deltaTime;
+
+            if(Vector3.Distance(parent.position, dashTarget) < 0.1f)
+            {
+                dashing = false;
+                rbody.isKinematic = false;
+            }
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Time.realtimeSinceStartup - lastCast > internalCD)
+            {
+                //cast
+                lastCast = Time.realtimeSinceStartup;
+                dashing = true;
+
+                rbody.isKinematic = true;
+
+                RaycastHit hit;
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+                Debug.DrawRay(ray.origin, ray.direction);
+
+                if (Physics.Raycast(ray, out hit, 60, floorLayer))
+                {
+                    dashTarget = hit.point;
+                    dashTarget.y += 1;
+
+                    if(Vector3.Distance(dashTarget, parent.position) > dashDistance)
+                    {
+                        Vector3 dashDirection = (dashTarget - parent.position).normalized;
+                        dashTarget = parent.position + dashDirection * dashDistance;
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Couldn't find floor to dash");
+                    dashing = false;
+                }
+
+                Debug.DrawRay(parent.position, dashTarget - parent.position, Color.green, 1);
+
+                parent.rotation = Quaternion.LookRotation(dashTarget - parent.position);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dashDistance);
+    }
+}
---
+++ SlasherMan/Assets/LivingThing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingThing : MonoBehaviour
+{
+    public int maxLife;
+
+    [SerializeField]
+    protected int life;
+
+    private void Start()
+    {
+        life = maxLife;
+    }
+
+    public void takeDamage(int amount)
+    {
+        life -= amount;
+        if(life <= 0)
+        {
+            //ded
+            Destroy(gameObject);
+        }
+    }
+}
