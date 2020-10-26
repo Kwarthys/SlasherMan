e2260d7e6aeedde40aab78610752b57a47984970
Kwarthys <Kwarthys@gmail.com>
2020-10-26 17:24:25 +0100

    Music integrated (but not synced here) and scales up with the monsters number

---
+++ SlasherMan/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+
+    public SpawnerManager monsterHolder;
+
+    public float volumeMin = .5f;
+    public float volumeMax = .9f;
+
+    void FixedUpdate()
+    {
+        float t = monsterHolder.transform.childCount * 1.0f / monsterHolder.maxMonsterCount;
+
+        musicSource.volume = Mathf.Lerp(volumeMin, volumeMax, t);
+    }
+}
